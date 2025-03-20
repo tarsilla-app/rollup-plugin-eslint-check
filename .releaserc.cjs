9585c6f1428a1c8343737057b46ce5c41e42a69e
theobassan <theobassan@gmail.com>
2025-03-20 10:05:39 -0300

    feat(release)!: major

---
+++ .releaserc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  branches: ['main'],
+  repositoryUrl: 'https://github.com/tarsilla-app/rollup-plugin-eslint-check',
+  plugins: ['@tarsilla/commit-wizard/semantic-release'],
+};
